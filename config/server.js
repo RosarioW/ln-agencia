0cc4d3ada5d5f443b81cb3b29e945fb3aa11bd4b
RosarioW <rosario@agencialosnavegantes.cl>
2021-05-10 20:34:13 -0400

    primer commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54c75ca69de2e20983495c7040a9cc3c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95f3ed55-1ebd-4309-a9a2-eb3eb918f332'
+};
